3bcd8567d21b3ee092e4516ab5ebae85642a2b04
Drew-Petty <78818551+Drew-Petty@users.noreply.github.com>
2021-08-01 20:55:54 -0500

    Update settings.py

--- adventures/settings.py
+++ adventures/settings.py
@@ -20,8 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'dd0z19t+s8$=+jdf+%ki+wm@*t7kmqflb@ltjuh!%b93#qz^0i'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
